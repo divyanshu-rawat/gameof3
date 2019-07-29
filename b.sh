87d091a81cb0f070c7f230a4b51736012d1e2bd6
Divyanshu <divyanshu.r46956@gmail.com>
2019-07-29 16:31:42 +0200

    Added playing algorithm in player 2.

--- b.sh
+++ b.sh
@@ -15,3 +15,44 @@ if [ $# -eq 1 ]; then
 fi
 
 echo "Player Name: $player_name"
+
+
+while true; do
+    ## read number sent from Player 1
+    read current < "$input"
+
+    ## the keyword "won" means that Player 1 has one.
+    if [[ "$current" -eq "won" ]]; then
+        echo
+        echo "LOSER: $player_name!"
+        exit
+    fi
+
+    echo "Number received: $current"
+
+    ## find the nearest number, that is divisible by 3.
+    ## Sbuilt-in command let for performing arithmetic operations.
+    if (( current % 3 == 0 )); then
+        let "next = ((current / 3))"
+    elif (( (current + 1) % 3 == 0 )); then
+        let "next = (((current + 1) / 3))"
+    elif (( (current - 1) % 3 == 0 )); then
+        let "next = (((current - 1) / 3))"
+    fi
+
+    echo "Number sent: $next"
+
+    ## next == 1, implies Player 2 has won!.
+    if (( next == 1 )); then
+        echo
+        echo "WINNER: $player_name!"
+        echo "won" > "$output"
+        exit
+    fi
+
+    ## else, Player 1 turn.
+    echo "$next" > "$output"
+
+done
+
+# set +x
