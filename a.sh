fd5864ebab57351354e144ea0ce3ed7983881bc0
Divyanshu <divyanshu.r46956@gmail.com>
2019-07-29 16:12:50 +0200

    Added script to generate random-number

--- a.sh
+++ a.sh
@@ -8,9 +8,22 @@ if [ $# -eq 0 ]; then
     player_name="player 1"
 fi
 
-## iff 1 argument then suggested name($1 grabs value of argument).
+## if 1 argument then suggested name($1 grabs value of argument).
 if [ $# -eq 1 ]; then
     player_name="$1"
 fi
 
 echo "Player Name: $player_name"
+
+## generate the random number.
+random=$(jot -r 1 10 99)
+
+echo "Random number: $random"
+
+## if the random number generated is 1, then game is already over.
+if (( random == 1 )); then
+    echo
+    echo "WINNER: $player_name!"
+    echo "won" > "$output"
+    exit
+fi
